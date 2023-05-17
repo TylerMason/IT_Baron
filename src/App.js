c5758016e79d88b9cb813b330ff26a38a7457298
TylerMason <tymasn@gmail.com>
2023-05-17 10:50:19 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+
+
+const { Header, Content, Footer } = Layout;
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
